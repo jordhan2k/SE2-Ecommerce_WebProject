2bc0ca1c72acd018dce73e304690a7350b5c2751
spane <spane@192.168.0.4>
2021-03-14 12:49:09 +0700

    Updated model.Category and model.Voucher

--- src/com/ecommerce/model/Category.java
+++ src/com/ecommerce/model/Category.java
@@ -1,5 +1,51 @@
 package com.ecommerce.model;
 
-public class Category {
+import java.io.Serializable;
+
+
+/**
+ * @overview 
+ *  Category is uses to sort products in different type
+ * @attributes
+ * <pre>
+ * categoryID   Integer int
+ * categoryName String
+ * </pre>
+ * @abstract_properties
+ * mutable(categoryID)=false /\ optional(categoryID)=false /\
+ * mutable(categoryName)=false /\ optional(categoryName)=false
+ * 
+ * @author TMDuong
+ *
+ */
+
+public class Category implements Serializable {
+	private int categoryID;
+	private String categoryName;
+
+	public Category() {
+	}
+
+	public Category(int categoryID, String categoryName) {
+		super();
+		this.categoryID = categoryID;
+		this.categoryName = categoryName;
+	}
+
+	public int getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
 
 }
--- src/com/ecommerce/model/Voucher.java
+++ src/com/ecommerce/model/Voucher.java
@@ -1,5 +1,78 @@
 package com.ecommerce.model;
 
-public class Voucher {
+import java.io.Serializable;
+import java.sql.Date;
 
+/**
+ * @overview 
+ *  Voucher is uses to decrease price in percentage
+ * @attributes
+ * <pre>
+ * voucherID   			Integer  	int
+ * voucherCode 			String
+ * discountPercentage	Integer		int
+ * expireDate			Date
+ * </pre>
+ * @abstract_properties
+ * mutable(voucherID)=false /\ optional(voucherID)=false /\ length(voucherID)=6 /\
+ * mutable(voucherCode)=true /\ optional(voucherCode)=false /\ length(voucherID)=10 /\
+ * mutable(discountPercentage)=true /\ optional(discountPercentage)=false /\ length(voucherID)=3 /\
+ * mutable(expireDate)=false /\ optional(expireDate)=false
+ * 
+ * @author TMDuong
+ *
+ */
+
+public class Voucher implements Serializable {
+
+	private int voucherID;
+	private String voucherCode;
+	private int discountPercentage;
+	private Date expireDate;
+	
+	public Voucher() {
+		super();
+	}
+
+	public Voucher(int voucherID, String voucherCode, int discountPercentage, Date expireDate) {
+		super();
+		this.voucherID = voucherID;
+		this.voucherCode = voucherCode;
+		this.discountPercentage = discountPercentage;
+		this.expireDate = expireDate;
+	}
+
+	public int getVoucherID() {
+		return voucherID;
+	}
+
+	public void setVoucherID(int voucherID) {
+		this.voucherID = voucherID;
+	}
+
+	public String getVoucherCode() {
+		return voucherCode;
+	}
+
+	public void setVoucherCode(String voucherCode) {
+		this.voucherCode = voucherCode;
+	}
+
+	public int getDiscountPercentage() {
+		return discountPercentage;
+	}
+
+	public void setDiscountPercentage(int discountPercentage) {
+		this.discountPercentage = discountPercentage;
+	}
+
+	public Date getExpireDate() {
+		return expireDate;
+	}
+
+	public void setExpireDate(Date expireDate) {
+		this.expireDate = expireDate;
+	}
+	
+	
 }
