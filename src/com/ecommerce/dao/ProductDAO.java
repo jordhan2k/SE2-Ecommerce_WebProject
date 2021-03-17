43a5f9f3d0e3b87c193fd47256e019ae4d8570ba
TMDuong <1801040054@s.hanu.edu.vn>
2021-03-17 21:49:36 +0700

    Update ProductDAO

--- src/com/ecommerce/dao/ProductDAO.java
+++ src/com/ecommerce/dao/ProductDAO.java
@@ -12,17 +12,58 @@ import com.ecommerce.model.Product;
  */
 
 public interface ProductDAO {
+	
+	/**
+	 * Insert a new Product to database
+	 * 
+	 * @param product - a thing, real-life object to be inserted to database
+	 */
 	void insertProduct(Product product);
 
+	/**
+	 * Update an existing Product in database
+	 * 
+	 * @param product - a thing, real-life object to be inserted to database
+	 * @return true if the product is updated, or false if not
+	 */
 	boolean updateProduct(Product product);
 
+	/**
+	 * Delete an existing Product
+	 * 
+	 * @param product - a thing, real-life object to be inserted to database
+	 * @return true if the product is deleted, or false if not
+	 */
 	boolean deleteProduct(int productID);
 
+	/**
+	 * Get a Product from database by its id
+	 * 
+	 * @param productID - the id of the desired Product
+	 * @return Either the desired Product if found or null if not
+	 */
 	Product getProductByID(int productID);
 
+	/**
+	 * Get all Products that exist in database
+	 * 
+	 * @return Either the list of Products or null if there is no Product
+	 */
 	List<Product> getAllProducts();
 
+	/**
+	 * Search for a Product by name
+	 * 
+	 * @param productName - the name to be searched for 
+	 * @return Either the list of matching result or null if there is no matches
+	 */
 	List<Product> searchProductByName(String productName);
 	
+	/**
+	 * Search for a Product by Category
+	 * 
+	 * @param categoryID - the ID to be searched for 
+	 * @return Either the list of matching result or null if there is no matches
+	 */
 	List<Product> searchProductByCategory(int categoryID);
 }
