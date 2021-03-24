22d42794a07fbb937d7744fe11d31545cc9924ca
Jordan Han <dunghoang1c.hanu@gmail.com>
2021-03-24 15:52:46 +0700

    Add productService

---
+++ src/com/ecommerce/service/impl/ProductServiceImpl.java
@@ -0,0 +1,81 @@
+package com.ecommerce.service.impl;
+
+import java.util.List;
+
+import com.ecommerce.dao.ProductDAO;
+import com.ecommerce.dao.impl.ProductDAOImpl;
+import com.ecommerce.model.Product;
+
+public class ProductServiceImpl {
+	
+	ProductDAO productDAO = new ProductDAOImpl();
+	
+	/**
+	 * Insert a new product
+	 * 
+	 * @param product - the product to be inserted to database
+	 */
+	public void insertProduct(Product product) {
+		productDAO.insertProduct(product);
+	}
+
+	/**
+	 * Update an existing product
+	 * 
+	 * @param product - the product to be updated
+	 * @return true if the product is updated, or false if not
+	 */
+	public boolean updateProduct(Product product) {
+		return productDAO.updateProduct(product);
+	}
+	
+
+	/**
+	 * Delete an existing product
+	 * 
+	 * @param productID - the id of the product to be deleted
+	 * @return true if the product is deleted, or false if not
+	 */
+	public boolean deleteProduct(int productID) {
+		return productDAO.deleteProduct(productID);
+	}
+
+	/**
+	 * Retreive a product by its id
+	 * 
+	 * @param productID - the id of the desired product
+	 * @return
+	 */
+	public Product getProductByID(int productID) {
+		return productDAO.getProductByID(productID);
+	}
+
+	/**
+	 * Retrieve all products
+	 * 
+	 * @return Either the list of all products or null if there is none
+	 */
+	public List<Product> getAllProducts(){
+		return productDAO.getAllProducts();
+	}
+
+	/**
+	 * Search for a product by its name
+	 * 
+	 * @param productName - the name of the desired product
+	 * @return Either the list of all matching products or null if there is none
+	 */
+	public List<Product> searchProductByName(String productName){
+		return productDAO.searchProductByName(productName);
+	}
+
+	/**
+	 * Search for a product by its category
+	 * @param categoryID - the id of the desired category
+	 * @return Either the list of all matching products or null if there is none
+	 */
+	public List<Product> searchProductByCategory(int categoryID){
+		return productDAO.searchProductByCategory(categoryID);
+	}
+
+}
