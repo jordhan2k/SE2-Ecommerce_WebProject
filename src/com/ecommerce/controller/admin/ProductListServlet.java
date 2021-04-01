1be4c4f15de00925cba91a6516785e8e11640d79
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-04-01 09:55:48 +0700

    Add ProductListServlet

---
+++ src/com/ecommerce/controller/admin/ProductListServlet.java
@@ -0,0 +1,41 @@
+package com.ecommerce.controller.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ecommerce.model.Product;
+import com.ecommerce.service.CategoryService;
+import com.ecommerce.service.ProductService;
+import com.ecommerce.service.impl.CategoryServiceImpl;
+import com.ecommerce.service.impl.ProductServiceImpl;
+
+
+
+
+
+@WebServlet("/admin/product/list")
+public class ProductListServlet extends HttpServlet{
+	ProductService productService = new ProductServiceImpl();
+		CategoryService cateService = new CategoryServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Product> productList = productService.getAllProducts();
+		req.setAttribute("product-list", productList);
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher(path);
+		
+	}
+		
+		
+	
+	
+	
+
+}
