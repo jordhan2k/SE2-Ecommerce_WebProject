b8e480bf56ae12e97412eab810957db35ce10859
TMDuong <1801040054@s.hanu.edu.vn>
2021-04-06 23:21:21 +0700

    Added Voucher Controllers

---
+++ src/com/ecommerce/controller/admin/VoucherAddServlet.java
@@ -0,0 +1,73 @@
+package com.ecommerce.controller.admin;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.ecommerce.model.Voucher;
+import com.ecommerce.service.VoucherService;
+import com.ecommerce.service.impl.VoucherServiceImpl;
+
+/**
+ * @overview VoucherAddServlet is a controller servlet used to handle the use
+ *           case of Adding a new Voucher to the database
+ * @author TMDuong
+ *
+ */
+@WebServlet(urlPatterns = "/admin/voucher/add")
+public class VoucherAddServlet extends HttpServlet {
+	VoucherService voucherService = new VoucherServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Voucher> vouchers = voucherService.getAllVouchers();
+		req.setAttribute("vouchers", vouchers);
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/view/admin/voucher-add.jsp");
+		requestDispatcher.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Voucher voucher = new Voucher();
+		// Create a factory for disk-based file items
+		DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+		// Create a new file upload handler
+		ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
+
+		try {
+			// parse the request to fileItems
+			List<FileItem> items = servletFileUpload.parseRequest(req);
+			for (FileItem item : items) {
+				if (item.getFieldName().equals("voucherCode")) {
+					voucher.setVoucherCode(item.getString());
+					System.out.println(item.getFieldName() + " - " + item.getString());
+				} else if (item.getFieldName().equals("discountPercent")) {
+					voucher.setDiscountPercentage(Integer.parseInt(item.getString()));
+					System.out.println(item.getFieldName() + " - " + item.getString());
+				} else if (item.getFieldName().equals("expireDate")) {
+					voucher.setExpireDate(Date.valueOf(item.getString()));
+					System.out.println(item.getFieldName() + " - " + item.getString());
+				}
+				voucherService.insertVoucher(voucher);
+				req.getRequestDispatcher(req.getContextPath() + "/admin/voucher/list").forward(req, resp);
+			}
+		} catch (FileUploadException e) {
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
