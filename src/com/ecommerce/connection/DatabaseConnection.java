fa1604a1038898355b6b533e27cf79b58da4253c
huubang-hanu <huubang.hanu@gmail.com>
2021-04-18 16:52:23 +0700

    refactor(DatabaseConnection): Allow public key rertrieval

--- src/com/ecommerce/connection/DatabaseConnection.java
+++ src/com/ecommerce/connection/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 	
-    private static String DB_URL = "jdbc:mysql://localhost:3306/fit_shop?useSSL=false";
+    private static String DB_URL = "jdbc:mysql://localhost:3306/fit_shop?useSSL=false&allowPublicKeyRetrieval=true";
     private static String USER_NAME = "root";
     private static String PASSWORD = "123456789";
 	
