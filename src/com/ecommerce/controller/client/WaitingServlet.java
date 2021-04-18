54bcf776d69a0eca78b818487d2e1e6a9c9c885e
huubang-hanu <huubang.hanu@gmail.com>
2021-04-18 17:09:36 +0700

    feat(controller): add WaitingServlet

---
+++ src/com/ecommerce/controller/client/WaitingServlet.java
@@ -0,0 +1,40 @@
+package com.ecommerce.controller.client;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ecommerce.model.User;
+
+
+/**
+ * Authorization role of account
+ * @author Admin
+ *
+ */
+@WebServlet("/waiting")
+public class WaitingServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		
+		if(session != null && session.getAttribute("account") != null) {
+			User user =(User) session.getAttribute("account");
+			req.setAttribute("username", user.getUsername());
+			if(user.getRoleID() == 1) { //If user is administrator
+				resp.sendRedirect(req.getContextPath()+"/admin"); //redirect to admin page
+			}else {
+				resp.sendRedirect(req.getContextPath()+"/welcome"); //redirect to Home page
+			}
+			
+		} else {
+			resp.sendRedirect(req.getContextPath()+"/login");
+		}
+		
+	}
+}
