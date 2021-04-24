58d46310a6650699dd12d874b37ea92a78d7a2a7
TMDuong <1801040054@s.hanu.edu.vn>
2021-04-24 22:51:48 +0700

    Added Productlist & Productdetail for customer

---
+++ src/com/ecommerce/controller/client/ProductDetailServlet.java
@@ -0,0 +1,39 @@
+package com.ecommerce.controller.client;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ecommerce.model.Category;
+import com.ecommerce.model.Product;
+import com.ecommerce.service.CategoryService;
+import com.ecommerce.service.ProductService;
+import com.ecommerce.service.impl.CategoryServiceImpl;
+import com.ecommerce.service.impl.ProductServiceImpl;
+
+@WebServlet(urlPatterns="/product/detail")
+public class ProductDetailServlet extends HttpServlet{
+	ProductService productService = new ProductServiceImpl();
+	CategoryService categoryService = new CategoryServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int productID = Integer.parseInt(req.getParameter("id"));
+		
+		Product product = productService.getProductByID(productID);
+		
+		List<Category> categories = categoryService.getAllCategories();
+		
+		req.setAttribute("categories", categories);
+		req.setAttribute("product", product);
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/customer/view/product-detail.jsp");
+		dispatcher.forward(req, resp);
+	}
+}
