26e2f2d8ca3f611e053549d5ef81ce6d91c3f134
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-04-25 13:02:20 +0700

    Update

--- src/com/ecommerce/controller/client/CustomerAccountServlet.java
+++ src/com/ecommerce/controller/client/CustomerAccountServlet.java
@@ -1,6 +1,7 @@
 package com.ecommerce.controller.client;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,20 +9,65 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ecommerce.model.User;
+import com.ecommerce.service.UserService;
+import com.ecommerce.service.impl.UserServiceImpl;
 
 @WebServlet("/customer/profile")
-public class CustomerAccountServlet extends HttpServlet{
-	
+public class CustomerAccountServlet extends HttpServlet {
+	UserService userService = new UserServiceImpl();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		RequestDispatcher rd = req.getRequestDispatcher("/view/customer/account-profile.jsp");
 		rd.forward(req, resp);
-		
-	} 
-	
+
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("Post called");
+
+		HttpSession session = req.getSession();
+
+		User account = (User) session.getAttribute("account");
+		// Unchangeable customer details
+		int userID = account.getUserID();
+
+		String username = account.getUsername();
+
+		String email = account.getEmail();
+
+		int roleID = account.getRoleID();
+
+		// Changeable
+		String mobile = req.getParameter("mobile");
+
+		String fullname = req.getParameter("fullname");
+
+		String gender = req.getParameter("gender");
+
+		Date dob = Date.valueOf(req.getParameter("dob"));
+
+		String address = req.getParameter("address");
+
+		String changePassword = req.getParameter("change-password");
+
+
+		String password = account.getPassword();
+		if (changePassword != null) {//			String oldPassword = req.getParameter("old-password");
+			String newPassword = req.getParameter("new-password");
+			System.out.println(newPassword);			
+			password = newPassword;
+		}
+		account = new User(userID, username, password, fullname, mobile, email, address, gender, dob, roleID);
+
+		userService.updateUser(account);
+
+		session.setAttribute("account", account);
+
+		resp.sendRedirect(req.getContextPath() + "/customer/profile");
 	}
 
 }
