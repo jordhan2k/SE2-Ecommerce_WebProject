a5f5313542e59d680ed4ed8e7d5620caefa07de8
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-05-09 21:08:30 +0700

    Update sql script

--- src/com/ecommerce/connection/lapeki_dbs.sql
+++ src/com/ecommerce/connection/lapeki_dbs.sql
@@ -15,6 +15,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP database IF EXISTS lapeki_dbs;
+create database lapeki_dbs;
+use lapeki_dbs;
+
+
 --
 -- Table structure for table `cart`
 --
